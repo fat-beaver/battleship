4d41e36705405af445659c8b283a6de3634e7fbb
jessica <57881206+fat-beaver@users.noreply.github.com>
2022-10-10 23:06:25 +1300

    Initial commit, rough feature parity with python version

---
+++ src/ai_player.rs
@@ -0,0 +1,98 @@
+use crate::game::{AimingBoard, BOARD_SIZE, Player, SHIP_LENGTHS, TargetBoard};
+
+use rand::distributions::WeightedIndex;
+use rand::prelude::*;
+
+const INITIAL_WEIGHT: u32 = 1000;
+const BASE_WEIGHT: u32 = 10;
+
+// used to determine whether a particular set of weights should be added to the output or not
+fn multiply_arrays(bool_arr: &[bool; BOARD_SIZE], num_arr: [u32; BOARD_SIZE]) -> [u32; BOARD_SIZE]{
+    let result: Vec<u32> = num_arr.iter().zip(bool_arr.iter()).map(|(num, b)| num * match b {true => 1, false => 0,}).collect();
+    return result.try_into().unwrap()
+}
+// used to remove cells which have been fired upon as legitimate targets
+fn multiply_arrays_inverse(bool_arr: &[bool; BOARD_SIZE], num_arr: [u32; BOARD_SIZE]) -> [u32; BOARD_SIZE]{
+    let result: Vec<u32> = num_arr.iter().zip(bool_arr.iter()).map(|(num, b)| num * match b {true => 0, false => 1,}).collect();
+    return result.try_into().unwrap()
+}
+
+// used to add weights to the weights total
+fn add_arrays(array1: [u32; BOARD_SIZE], array2: [u32; BOARD_SIZE]) -> [u32; BOARD_SIZE]{
+    let array3: Vec<u32> = array1.iter().zip(array2.iter()).map(|(a, b)|a + b).collect();
+    return array3.try_into().unwrap();
+}
+
+// struct to record which actions have been taken with particular inputs, for adjusting weights
+struct Action {
+    hits_input: [bool; BOARD_SIZE],
+    misses_input: [bool; BOARD_SIZE],
+    shot_taken: usize
+}
+
+impl Action {
+    fn new(hits: [bool; BOARD_SIZE], misses: [bool; BOARD_SIZE], shot: usize) -> Self {
+        Self {
+            hits_input: hits,
+            misses_input: misses,
+            shot_taken: shot
+        }
+    }
+}
+
+pub struct AIPlayer {
+    base_weights: [u32; BOARD_SIZE],
+    hits_weights: [[u32; BOARD_SIZE]; BOARD_SIZE],
+    misses_weights: [[u32; BOARD_SIZE]; BOARD_SIZE],
+    possible_shots: [usize; BOARD_SIZE],
+    actions: Vec<Action>
+}
+
+impl AIPlayer {
+    pub fn new() -> Self {
+        Self {
+            base_weights: [BASE_WEIGHT; BOARD_SIZE],
+            hits_weights: [[INITIAL_WEIGHT; BOARD_SIZE]; BOARD_SIZE],
+            misses_weights: [[INITIAL_WEIGHT; BOARD_SIZE]; BOARD_SIZE],
+            possible_shots: core::array::from_fn(|i| i),
+            actions: vec![]
+        }
+    }
+}
+
+impl Player for AIPlayer {
+    fn new_game(mut self: &mut AIPlayer) {
+        self.actions = vec![]
+    }
+
+    fn place_ships(self: AIPlayer) -> TargetBoard {
+        // reset actions because place_ships is called at the start of each game
+        let mut target_board: TargetBoard = TargetBoard::new();
+        for i in 0..SHIP_LENGTHS.len() {
+            target_board.place_ship(i as u32, 0, SHIP_LENGTHS[i], true);
+        }
+        return target_board;
+    }
+
+    fn take_shot(mut self: &mut AIPlayer, aiming_board: &AimingBoard) -> usize{
+        // add an initial weight to each cell
+        let mut shot_weights = self.base_weights.clone();
+        // determine weights for each cell based on hits and misses
+        for cell_number in 0..BOARD_SIZE {
+            shot_weights = add_arrays(shot_weights, multiply_arrays(aiming_board.get_hits(), self.hits_weights[cell_number]));
+            shot_weights = add_arrays(shot_weights, multiply_arrays(aiming_board.get_misses(), self.misses_weights[cell_number]));
+        }
+        // remove cells which have been shot at already
+        shot_weights = multiply_arrays_inverse(aiming_board.get_hits(), shot_weights);
+        shot_weights = multiply_arrays_inverse(aiming_board.get_misses(), shot_weights);
+        // use weightedIndex to choose a shot to take based on the random weights which have been generated
+        let chosen_shot: usize = self.possible_shots[WeightedIndex::new(shot_weights).unwrap().sample(&mut thread_rng())];
+        // record the action taken to use it for adjusting weights later
+        self.actions.push(Action::new(aiming_board.get_hits().clone(), aiming_board.get_misses().clone(), chosen_shot));
+        return chosen_shot;
+    }
+
+    fn game_finish(mut self: &mut AIPlayer, won: bool) {
+
+    }
+}
---
+++ src/game.rs
@@ -0,0 +1,159 @@
+pub const BOARD_WIDTH: u32 = 10;
+pub const BOARD_HEIGHT: u32 = 10;
+pub const BOARD_SIZE: usize = (BOARD_WIDTH * BOARD_HEIGHT) as usize;
+
+pub const SHIP_LENGTHS: [u32; 5] = [5, 4, 3, 3, 2];
+
+pub const TOTAL_SHIP_HEALTH: u32 = 17;  // the total of the ship lengths
+
+#[derive(Clone)]
+pub struct AimingBoard {
+    hits: [bool; BOARD_SIZE],
+    misses: [bool; BOARD_SIZE]
+}
+
+impl AimingBoard {
+    fn new() -> Self {
+        Self {
+            hits: [false; BOARD_SIZE],
+            misses: [false; BOARD_SIZE]
+        }
+    }
+    pub fn get_hits(&self) -> &[bool; BOARD_SIZE] {
+        return &self.hits;
+    }
+    pub fn get_misses(&self) -> &[bool; BOARD_SIZE] {
+        return &self.misses;
+    }
+}
+
+pub struct Ship {
+    x_coord: u32,
+    y_coord: u32,
+    length: u32,
+    vertical: bool
+}
+impl Ship {
+    pub fn new(x: u32, y: u32, len: u32, vert: bool) -> Self {
+        Self {
+            x_coord: x,
+            y_coord: y,
+            length: len,
+            vertical: vert
+        }
+    }
+}
+
+pub struct TargetBoard {
+    ships: Vec<Ship>,
+    ships_required: Vec<u32>,
+    cells_containing_ships: [bool; BOARD_SIZE]
+}
+
+impl TargetBoard {
+    pub fn new() -> Self {
+        Self {
+            ships: vec![],
+            ships_required: Vec::from(SHIP_LENGTHS),
+            cells_containing_ships: [false; BOARD_SIZE]
+        }
+    }
+    pub fn place_ship(&mut self, x_coord: u32, y_coord: u32, ship_length: u32, vertical: bool) {
+        if self.ships_required.contains(&ship_length) &&
+            x_coord < BOARD_WIDTH && y_coord < BOARD_HEIGHT {
+            // remove the ship from the list of ships to add and add it to the list of ships
+            self.ships_required.remove(self.ships_required.iter().position(|x| *x == ship_length).unwrap());
+            // create a ship object and add it to the target board
+            let new_ship: Ship = Ship::new(x_coord, y_coord, ship_length, vertical);
+            self.ships.push(new_ship);
+            // determine which cells this ship occupies
+            if vertical {
+                for i in 0..ship_length {
+                    self.cells_containing_ships[((i + y_coord) * BOARD_WIDTH + x_coord) as usize] = true;
+                }
+            } else {
+                for i in 0..ship_length {
+                    self.cells_containing_ships[(y_coord * BOARD_WIDTH + x_coord + i) as usize] = true;
+                }
+            }
+        }
+    }
+    pub fn check_hit(&self, cell_number: usize) -> bool{
+        return self.cells_containing_ships[cell_number];
+    }
+}
+
+pub trait Player {
+    fn new_game(&mut self);
+    fn place_ships(&mut self) -> TargetBoard;
+    fn take_shot(&mut self, aiming_board: &AimingBoard) -> usize;
+    fn game_finish(&mut self, won: bool);
+}
+
+struct InternalPlayer {
+    player: Box<dyn Player>,
+    hits_left: u32,
+    aiming_board: AimingBoard,
+    target_board: TargetBoard
+}
+
+impl InternalPlayer {
+    fn new(mut p: &Box<dyn Player>) -> Self {
+        Self {
+            target_board: p.place_ships(),
+            player: p,
+            hits_left: TOTAL_SHIP_HEALTH,
+            aiming_board: AimingBoard::new()
+        }
+    }
+}
+
+struct BattleshipGame {
+    player1: Box<dyn Player>,
+    player2: Box<dyn Player>
+}
+
+impl BattleshipGame {
+    fn new(p1: Box<dyn Player>, p2: Box<dyn Player>) -> BattleshipGame {
+        Self {
+            player1: p1,
+            player2: p2
+        }
+    }
+    pub fn run_game(&self) {
+        // create an array to hold the players so they can be alternated between easily
+        let mut players: [InternalPlayer; 2] = [InternalPlayer::new(&self.player1), InternalPlayer::new(&self.player2)];
+
+        let mut current_player_id: usize = 0;
+        let mut _turns_taken: usize = 0;
+
+        // continue running until a player has lost
+        while players[0].hits_left > 0 && players[1].hits_left > 0 {
+            // increase the counter of turns taken each time player 1 takes their turn
+            if current_player_id == 0 {
+                _turns_taken += 1;
+            }
+            // ask the current player to take a shot with the information in their aiming board
+            let shot_taken: usize = players[current_player_id].player.take_shot(&players[current_player_id].aiming_board);
+            if players[(current_player_id + 1).rem_euclid(2)].target_board.check_hit(shot_taken) {
+                players[current_player_id].aiming_board.hits[shot_taken] = true;
+                // take one hit away from the player who was hit
+                players[(current_player_id + 1).rem_euclid(2)].hits_left -= 1;
+            } else {
+                players[current_player_id].aiming_board.hits[shot_taken] = false;
+            }
+            // switch to the other player
+            current_player_id = (current_player_id + 1).rem_euclid(2);
+        }
+        // inform the players of their victory/loss when the game ends
+        if players[0].hits_left == 0 {
+            players[0].player.game_finish(false);
+            players[1].player.game_finish(true);
+        } else {
+            players[0].player.game_finish(true);
+            players[1].player.game_finish(false);
+        }
+    }
+
+
+}
