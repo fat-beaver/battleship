2d52289baa016fd73c4d077a13caa1d5d7fbeea7
jessica <57881206+fat-beaver@users.noreply.github.com>
2022-10-11 03:04:58 +1300

    Changed number of games each thread plays

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::sync::mpsc;
 use std::thread;
-use std::thread::{JoinHandle};
+
 use std::time::Instant;
 use ai_player::AIPlayer;
 use game::BattleshipGame;
@@ -9,7 +9,7 @@ mod game;
 mod ai_player;
 
 const PARALLEL_GAMES: usize = 4;
-const SERIAL_GAMES: usize = 100;
+const SERIAL_GAMES: usize = 1000;
 
 fn main() {
     let parallel_games = num_cpus::get() - 1;
@@ -41,7 +41,7 @@ fn main() {
     for mut game in rx {
         games_played += parallel_games * SERIAL_GAMES;
         let duration = Instant::now().duration_since(start_time).as_secs_f64();
-        println!("{} games played in {}s, {} per second", games_played, duration, games_played as f64 / duration as f64);
+        println!("{} games played in {}s, {} per second", games_played, duration, games_played as f64 / duration);
         let tx1 = tx.clone();
         thread::spawn(move || {
             game.run_multiple(SERIAL_GAMES);
